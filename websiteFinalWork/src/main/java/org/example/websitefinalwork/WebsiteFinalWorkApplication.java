5c030bce96d5cd16d47705a7aa59ba88686461c8
Henrique3040 <henrique.de.sousa.lima.cardoso@student.ehb.be>
2025-02-22 15:56:48 +0100

    initial comit

---
+++ websiteFinalWork/src/main/java/org/example/websitefinalwork/WebsiteFinalWorkApplication.java
@@ -0,0 +1,13 @@
+package org.example.websitefinalwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteFinalWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteFinalWorkApplication.class, args);
+	}
+
+}
---
+++ websiteFinalWork/src/test/java/org/example/websitefinalwork/WebsiteFinalWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.websitefinalwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteFinalWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
